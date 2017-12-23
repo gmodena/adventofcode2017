325cc1733c4a975f21eaa21fd817a1a5057fab34
Gabriele Modena <gm@nowave.it>
2017-12-23 23:39:23 +0100

    Day 22 part 2

--- 23/solution.py
+++ 23/solution.py
@@ -1,16 +1,16 @@
 from collections import defaultdict
+from math import sqrt
 
-registers = defaultdict(int)
+def part1():
+    registers = defaultdict(int)
 
-def get_value(registers, v):
-    try:
-        return int(v)
-    except ValueError:
-        return registers[v]
-
-if __name__ == '__main__':
+    def get_value(registers, v):
+        try:
+            return int(v)
+        except ValueError:
+            return registers[v]
     op = 0; reg = 1; val = 2
-    with open('input.txt') as infile:
+    with open('input.opt.txt') as infile:
         instructions = [line.strip().split(' ') for line in infile]
 
     count = 0
@@ -30,4 +30,22 @@ if __name__ == '__main__':
                 pc += get_value(registers, ins[val])
                 continue
         pc += 1
-    print(count)
+    return count
+
+def part2():
+    def is_prime(n):
+        for i in range(2, int(sqrt(n))):
+            if n % i == 0:
+                return False
+        return True
+
+    c = 123700
+    h = 0
+    for b in range(106700, c+1, 17):
+        if not is_prime(b):
+            h += 1
+    return h
+
+if __name__ == '__main__':
+    print(part1())
+    print(part2())
