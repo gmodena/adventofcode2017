be7cd7db22ddd8bd660c83e1d3366e235391759a
Gabriele Modena <gm@nowave.it>
2017-12-25 01:00:59 +0100

    Day 24 refactoring

--- 24/solution.py
+++ 24/solution.py
@@ -1,35 +1,34 @@
 from collections import defaultdict
 import operator
+def find_bridges(components):
+    def dfs(bridge, components):
+        # current node
+        cur = bridge[-1][1]
+        for node in components[cur]:
+            if not ((cur, node) in bridge or (node, cur) in bridge):
+                new_bridge = bridge + [(cur, node)]
+                yield new_bridge
+                yield from dfs(new_bridge, components)
 
-def dfs(bridge, components):
-    # current node
-    cur = bridge[-1][1]
-    for node in components[cur]:
-        if (cur, node) not in bridge and (node, cur) not in bridge:
-            new_bridge = bridge + [(cur, node)]
-            yield new_bridge
-            yield from dfs(new_bridge, components)
-
-def part1(bridges):
-    s = []
-    for bridge in bridges:
-        s.append(sum([x+y for x, y in bridge]))
-    return max(s)
-
-def part2(bridges):
+    bridges = list(dfs([(0, 0)], components))
     s = []
     for bridge in bridges:
         s.append((len(bridge), sum([x+y for x, y in bridge])))
-    return sorted(s, key = operator.itemgetter(0, 1), reverse=True)[0]
+    return s
+
+def part1(bridges):
+    return sorted(bridges, key = operator.itemgetter(1), reverse=True)[0][1]
+def part2(bridges):
+    return sorted(bridges, key = operator.itemgetter(0, 1), reverse=True)[0][1]
 
 if __name__ == '__main__':
     components = defaultdict(set)
-
-
     with open('input.txt') as infile:
         for row in infile.readlines():
-            row = list(map(int, row.strip().split('/')))
-            components[row[0]].add(row[1])
-            components[row[1]].add(row[0])
-    bridges = list(dfs([(0, 0)], components))
+            x, y = list(map(int, row.strip().split('/')))
+            components[x].add(y)
+            components[y].add(x)
+
+    bridges = find_bridges(components)
+    print(part1(bridges))
     print(part2(bridges))
