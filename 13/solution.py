1f481f3ba1f3ccdb1426828d1e847bdd37c4139f
Gabriele Modena <gm@nowave.it>
2017-12-26 22:39:01 +0100

    Day 13 part b

--- 13/solution.py
+++ 13/solution.py
@@ -1,4 +1,28 @@
-from collections import defaultdict
+def is_caught(layers, time, delay=0):
+    layer_pos = (time+delay) % (2 * (layers[time] - 1))
+    if layer_pos == 0:
+        return True
+    return False
+
+def part1(layers):
+    severity = 0
+    for time in layers:
+        if is_caught(layers, time):
+            severity += time * layers[time]
+    return severity
+
+def part2(layers):
+    delay = 0
+    while True:
+        caught = False
+        for time in layers:
+            if is_caught(layers, time, delay):
+                caught = True
+                break
+        if not caught:
+            break
+        delay += 1
+    return delay
 
 if __name__ == '__main__':
     layers = {}
@@ -6,10 +30,5 @@ if __name__ == '__main__':
         for row in infile.readlines():
             x, y = row.strip().split(':')
             layers[int(x.strip())] = int(y.strip())
-
-    severity = 0
-    for time in layers:
-        layer_pos = time % (2 * (layers[time] - 1))
-        if layer_pos == 0:
-            severity += time * layers[time]
-    print(severity)
+    print(part1(layers))
+    print(part2(layers))
